67227269586953c09b11f573d5e2b198f6e7c333
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-10-17 17:27:32 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("landing");
+})
+
+app.get("/campgrounds", function(req, res){
+  var campgrounds = [
+    {name:"Salmon Creek", image: "https://www.photosforclass.com/download/pixabay-1149402?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c722e78d59245c158_960.jpg&user=Free-Photos"},
+    {name:"Granite Hill", image: "https://www.photosforclass.com/download/pixabay-3616194?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F55e6d4454b5ba814f6da8c7dda793f7f1636dfe2564c704c722e78d1904bc25f_960.jpg&user=Schwoaze"},
+    {name:"Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-1189929?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c722e78d59245c158_960.jpg&user=Noel_Bauza"},
+    {name:"Trout Bay", image: "https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c722e78d59245c158_960.jpg&user=Pexels"},
+  ]
+  res.render("campgrounds", {campgrounds: campgrounds});
+})
+
+app.listen(PORT, () => {
+    console.log(`YelpCamp is running on port ${ PORT }`);
+});
