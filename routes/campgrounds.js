97c68876dca4fdf27ed73bcffc31aca309ae398c
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-10-31 22:23:51 -0700

    Seperate routes

---
+++ routes/campgrounds.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var router  = express.Router();
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// INDEX — Displays all campgrounds
+router.get("/", function(req, res){
+  // Get all campgrounds from DB 
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds/index", {campgrounds: allCampgrounds});
+    }
+  });
+});
+
+// NEW — Displays form to add campground
+router.get("/new", function(req, res){
+  res.render("campgrounds/new");
+}); 
+  
+// CREATE — Adds new campground                                                                    
+router.post("/", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image, description: description};
+  
+  // Create new campground and save to the DB 
+  Campground.create(newCampground, function(err, campground){
+    if(err){
+      console.log(err);
+    } else {
+      // Redirect
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
+// SHOW — Displays information of one campground
+router.get("/:id", function(req, res){
+  // Find campground with provided ID
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds/show",{campground: foundCampground});
+    }
+  });
+});
+
+module.export = router;
---
+++ routes/index.js
@@ -0,0 +1,67 @@
+var express  = require("express");
+var router   = express.Router();
+var passport = require("passport");
+var User     = require("../models/user");
+
+
+// Landing page
+router.get("/", function(req, res){
+  res.render("landing");
+});
+
+// ================
+// Authentication Route
+// ================
+router.get("/register", function(req, res){
+  res.render("register");
+});
+
+router.post("/register", function(req, res){
+   newUser = new User({username: req.body.username});
+   User.register(newUser, req.body.password, function(err, user){
+     if(err){
+       console.log(err);
+       return res.render("register");
+     }
+     passport.authenticate("local")(req, res, function(){
+       res.redirect("/campgrounds");
+     })
+   })
+});
+
+// ============
+// Login Routes
+// ============
+
+// Show login form
+router.get("/login", function(req, res){
+  res.render("login");
+});
+
+router.post("/login", passport.authenticate("local",{
+  // Middleware — code that runs before the callback
+  successRedirect: "/campgrounds",
+  failureRedirect: "/login"
+}), function(req, res){
+  
+});
+
+// ============
+// Logout Routes
+// ============
+router.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/campgrounds");
+});
+
+// ============
+// Is Logged In Check
+// ============
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  } 
+  res.redirect("/login");
+}
+
+module.export = router;
