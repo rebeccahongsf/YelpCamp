e9953f0a93a25bb2d0c253763cd94c67caea8c63
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-11-05 17:17:32 -0800

    Implement Comment Destory route

--- routes/comments.js
+++ routes/comments.js
@@ -43,21 +43,41 @@ router.post("/", function(req, res){
   })
 });
 
-// Edit comment
+// Edit Comment
 router.get("/:comment_id/edit", function(req, res){
-  Campground.findById(req.params.id, function(err, campground){
+  Comment.findById(req.params.comment_id, function(err, foundComment){
     if(err){
       console.log(err);
     } else {
-      res.render("comments/edit", {campground: campground});
+      res.render("comments/edit", {campground_id: req.params.id, comment: foundComment} );
     }
   });
 });
 
-// Update
-// router.put("/", function(req, res){
-// 
-// });
+// Update Comment
+router.put("/:comment_id", function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+    if(err){
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  })
+});
+
+// Delete Comment
+router.delete("/:comment_id", function(req, res){
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err){
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id );
+    }
+  });
+});
+
 
 // ===================
 // Is Logged In Check
